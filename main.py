00e4f0d31fdcd8ea7e7456aa37148933af1ba64d
Pingüi Maya <75802476+PinguiMaya@users.noreply.github.com>
2023-04-19 10:51:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -108,5 +108,5 @@ def pagenotfound(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=False,host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False, host="0.0.0.0", port=8080)
 
